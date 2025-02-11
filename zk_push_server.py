7a4d5a1711a67be4dd3c5aa51d37b7b2ed620371
aconstanza16 <anthonyconstanza1@gmail.com>
2025-02-11 17:52:46 -0400

    Update zk_push_server.py

--- zk_push_server.py
+++ zk_push_server.py
@@ -27,5 +27,3 @@ def device_status():
     except requests.exceptions.RequestException as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
